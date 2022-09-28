618ac7493ff22e584d8d6632909a4e3090325055
mimmarry <105546871+mimmarry@users.noreply.github.com>
2022-09-28 23:11:45 +0300

    Add files via upload

---
+++ треугольник.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace df;
+class Programm
+{
+    static void Main(string[] args)
+    {
+         int a = 5;
+        /*int b = 7; 
+         double s = 0.5 * a * b;
+        Задание найти площадь треугольника сданными сторонами:
+        Console.WriteLine(s);
+        Задание найти гипотенузу по теореме Пифагорова
+        double c1 = (Math.Pow(a, 2)) + (Math.Pow(b, 2));
+        double c2 = Math.Sqrt(c1);
+        Console.WriteLine(c2);
+        ЗАДАНИЕ НАЙТИ ГИПОТЕНУЗУ ПО ФОРМУЛЕ ЧЕРЕЗ СИНУС*/
+        double c = a / Math.Sin(90);
+        Console.WriteLine(c);
+    }
+}
